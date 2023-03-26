13bc607d966ecdefccb8523d7acb715316ceaea4
Abimbola Michael <abimbolamichael100@gmail.com>
2023-03-26 07:10:58 -0700

    Get Precision

---
+++ get_precision.c
@@ -0,0 +1,41 @@
+#include "maih.h"
+
+/**
+ * get_precision - gets precision
+ * @format: input char pointer
+ * @i: lists of args to print
+ * @list: list of args
+ * Return: precision value
+ */
+
+int get_precision(const char *format, int *i, va_list list)
+{
+	int i = *i + 1;
+	int precision = -1;
+
+	if (format[i] != '.')
+		return (precision);
+
+	precision = 0;
+
+	for (i += 1; format[i] != '\0'; i++)
+	{
+		if (is_digit(format[i]))
+		{
+			precision *= 10;
+			precision += format[i] - '0';
+		}
+		else if (format[i] == '*')
+		{
+			i++;
+			precision = va_arg(list, int);
+			break;
+		}
+		else
+			break;
+	}
+
+	*i = i - 1;
+
+	return (precision);
+}
