30f4b64afe55bb1c3666d4efc02ef06934dd033b
Abimbola Michael <abimbolamichael100@gmail.com>
2023-03-26 08:50:07 -0700

    Function c correction

--- functions.c
+++ functions.c
@@ -37,7 +37,7 @@ int print_char(va_list types, char buffer[],
 int print_string(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int length = 0;
+	int len = 0;
 	int i;
 	char *str = va_arg(types, char *);
 
@@ -52,35 +52,31 @@ int print_string(va_list types, char buffer[],
 		if (precision >= 6)
 			str = "      ";
 	}
-
-	while (str[length] != '\0')
-		length++;
-
-	if (precision >= 0 && precision < length)
-		length = precision;
-
-	if (width > length)
+	while (str[len] != '\0')
+		len++;
+	if (precision >= 0 && precision < len)
+		len = precision;
+	if (width > len)
 	{
 		if (flags & F_MINUS)
 		{
-			write(1, &str[0], length);
-			for (i = width - length; i > 0; i--)
+			write(1, &str[0], len);
+			for (i = width - len; i > 0; i--)
 				write(1, " ", 1);
 			return (width);
 		}
 		else
 		{
-			for (i = width - length; i > 0; i--)
+			for (i = width - len; i > 0; i--)
 				write(1, " ", 1);
-			write(1, &str[0], length);
+			write(1, &str[0], len);
 			return (width);
 		}
 	}
-
-	return (write(1, str, length));
+	return (write(1, str, len));
 }
 
-/****************** PRINT PERCENT SIGN ********************/
+/************** PRINT PERCENT SIGN ***************/
 
 /**
  * print_percent - Prints a percent sign
