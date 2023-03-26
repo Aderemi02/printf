64142f2b3436ed393d6263862c4595c3a98ee67b
Aderemi <omoremilekun@gmail.com>
2023-03-26 05:14:16 -0700

    _printf.c

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*****STANDARD LIBRARIES****/
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**** PROTOTYPES OF FUNCTIONS USED***/
+
+void print_buffer(char buffer[], int *buff_ind);
