b60718717125febe67cdae7d90d4e6063a1fd3f4
Aderemi <omoremilekun@gmail.com>
2023-03-27 14:07:19 -0700

    checking errors

--- functions.c
+++ functions.c
@@ -41,6 +41,7 @@ int print_string(va_list types, char buffer[],
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
+
 	if (str == NULL)
 	{
 		str = "(null)";
@@ -53,7 +54,6 @@ int print_string(va_list types, char buffer[],
 
 	if (precision >= 0 && precision < len)
 		len = precision;
-	
 	if (width > len)
 	{
 		if (flags & F_MINUS)
