5d41cd24a19ba70f1b7d6b25d98db6df070f62a7
Aderemi <omoremilekun@gmail.com>
2023-03-27 11:57:25 -0700

    updating

--- hnd_prting.c
+++ hnd_prting.c
@@ -22,7 +22,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal},
 		{'x', print_hexadecimal}, {'X', print_hexa_upper},
-		{'p', printp_pointer}, {'S', print_non_printable},
+		{'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string},
 		{'\0', NULL}
 	};
